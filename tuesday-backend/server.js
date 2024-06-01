10908f6fa5877e8b54e5a5db2e8bf8b4f80ec525
Ethan Bosen <ethbosen@gmail.com>
2024-06-01 15:39:14 -0400

    backend

---
+++ tuesday-backend/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const helmet = require('helmet');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Security middleware
+app.use(helmet());
+
+// Logging middleware
+app.use(morgan('combined'));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes for the buttons
+app.get('/booking', (req, res) => {
+    res.redirect('music:');
+});
+
+app.get('/social', (req, res) => {
+    res.redirect('https://www.instagram.com/tuesdaydub/');
+});
+
+app.get('/payment', (req, res) => {
+    res.redirect('https://www.paymentsite.com');
+});
+
+app.get('/merch', (req, res) => {
+    res.redirect('https://www.merchsite.com');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
