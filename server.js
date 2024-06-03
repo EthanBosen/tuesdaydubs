14f953794924af56014e29c5b22fcbee7406e576
Ethan Bosen <ethbosen@gmail.com>
2024-06-03 16:05:43 -0400

    back end complete

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Security middleware
-app.use(helmet());
-
-// Logging middleware
-app.use(morgan('combined'));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
